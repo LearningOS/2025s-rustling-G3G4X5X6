a548085214673b1d7ab8dc219505edac65645132
G3G4X5X6 <87740076+G3G4X5X6@users.noreply.github.com>
2025-03-30 08:38:14 +0000

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        
+        let v: Vec<&str> = s.split(',').collect();
+
+        if v.len() ==  2 {
+
+            let name = v[0].to_string();
+            if name.len()==0 {
+
+                return Person::default();
+            }
+
+            let age = v[1].to_string();
+            return match age.parse() {
+                Ok(age) => Person { name: name.to_string(), age },
+                Err(_) => Person::default(),
+            }
+        } 
+        Person::default()
+        
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        
+        let v: Vec<&str> = s.split(',').collect();
+
+        if v.len() ==  2 {
+
+            let name = v[0].to_string();
+            if name.len()==0 {
+                return Err(ParsePersonError::NoName)
+            }
+
+            let age = v[1].to_string();
+            return match age.parse() {
+                Ok(age) => Ok(Person { name: name.to_string(), age }),
+                Err(error) => Err(ParsePersonError::ParseInt(error)),
+            }
+        } 
+        Err(ParsePersonError::BadLen)
     }
 }
 
